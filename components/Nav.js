8789422a57d178350117a1714b55da27a5785ebb
Joseph Simpson <joseph.simpson@thehomeslicegroup.com>
2019-12-19 13:40:42 -0700

    Update nav to include submenus

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from '../utils/ActiveLink'
 import mainTheme from '../styles/katTheme'
-import { useSelector } from "react-redux"
+import {useSelector} from "react-redux"
 import {withRedux} from "../lib/redux"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 
@@ -38,26 +38,39 @@ const Nav = () => {
         color: black;
         text-decoration: none;
       }
+      nav {
+        text-align: center;
+        background: ${mainTheme.brand};
+        display: none;
+      }
+      @media all and (min-width: ${mainTheme.menuBreakPoint}){
+        nav{
+          display: block;
+        }
+      }
     `}</style>
     </nav>
   }
 
   const links = navItems.navItems.map(navItem => {
-      let navItemInfo
+      let navItemInfo = {
+        label: navItem.title,
+        type: navItem.type,
+        childItems: navItem.child_items ?
+          navItem.child_items.map(childItem => {
+            const parentSlug = childItem.object === 'page' || childItem.object === 'custom' ?
+              ''
+              : '/' + navItem.slug
+            return Object.assign({}, childItem, {parentSlug: parentSlug})
+          })
+          : null
+      }
       if (navItem.type === 'custom') {
-        navItemInfo = {
-          key: navItem.url,
-          href: navItem.url,
-          label: navItem.title,
-          type: navItem.type
-        }
+        navItemInfo.key = navItem.url
+        navItemInfo.href = navItem.url
       } else {
-        navItemInfo = {
-          key: navItem.slug,
-          href: '/' + navItem.slug,
-          label: navItem.title,
-          type: navItem.type
-        }
+        navItemInfo.key = navItem.slug
+        navItemInfo.href = '/' + navItem.slug
       }
       return (
         navItemInfo
@@ -68,12 +81,39 @@ const Nav = () => {
   return (
     <nav>
       <MaxWidthWrapper>
-        <ul>
-          {links.map(({key, href, label, type}) => {
+        <ul className="top-level-menu">
+          {links.map(({key, href, label, type, childItems}) => {
+              const subMenu = childItems ?
+                <ul className="sub-menu">
+                  {childItems.map(item => {
+                    if (item.type === 'custom') {
+                      return (
+                        <li key={item.url}>
+                          <a href={item.url}>
+                            {item.title}
+                          </a>
+                        </li>
+                      )
+                    } else {
+                      return (
+                        <li key={item.url}>
+                          <Link activeClassName=" active-link" href={`${item.parentSlug}/${item.slug}`}>
+                            <a>
+                              {item.title}
+                            </a>
+                          </Link>
+                        </li>
+                      )
+                    }
+                  })}
+                </ul>
+                : ''
+
               if (type === 'custom') {
                 return (
                   <li key={key}>
                     <a href={href}>{label}</a>
+                    {subMenu}
                   </li>
                 )
               } else {
@@ -82,6 +122,7 @@ const Nav = () => {
                     <Link activeClassName=" active-link" href={href}>
                       <a>{label}</a>
                     </Link>
+                    {subMenu}
                   </li>
                 )
               }
@@ -97,27 +138,31 @@ const Nav = () => {
       }
       nav {
         text-align: center;
-        background: ${ mainTheme.brand };
+        background: ${mainTheme.brand};
         display: none;
       }
-      @media all and (min-width: ${ mainTheme.menuBreakPoint }){
+      @media all and (min-width: ${mainTheme.menuBreakPoint}){
         nav{
           display: block;
         }
       }
-      ul {
+      .top-level-menu {
         display: flex;
         justify-content: space-between;
         width: 100%;
       }
-      nav > ul {
+      nav > .top-level-menu {
         padding: 4px 16px;
+      }
+      .top-level-menu li{
+        position: relative;
       }
       li {
         display: flex;
         padding: 6px 8px;
       }
       a {
+        border-bottom: transparent 2px solid;
         font-weight: bold;
         color: black;
         text-decoration: none;
@@ -128,6 +173,21 @@ const Nav = () => {
       }
       .active-link {
         border-bottom: black solid 2px;
+      }
+      .top-level-menu li:hover .sub-menu, .top-level-menu li:focus-within .sub-menu{
+        display: block;
+      }
+      .sub-menu{
+        background: white;
+        position: absolute;
+        display: none;
+        z-index: 500;
+        text-align: center;
+        left: 50%;
+        transform: translate(-50%, 23px);
+      }
+      .sub-menu a{
+        width: 100%;
       }
     `}</style>
     </nav>
