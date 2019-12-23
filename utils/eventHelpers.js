b1cb45681d382ff7434d21ef8d7512f139728a45
Joseph Simpson <joseph.simpson@thehomeslicegroup.com>
2019-12-23 15:25:36 -0700

    Fix timezones for events

--- utils/eventHelpers.js
+++ utils/eventHelpers.js
@@ -1,13 +1,19 @@
+const timestamp = (date = null) => {
+  return  date ?
+    Math.round((new Date(date).getTime() /1000))
+    : Math.round((new Date().getTime() /1000))
+}
+
 export const activeItemsOnly = items => {
   return items.filter(
     item => {
-      const displayStartTime = !parseInt(item.meta_box.event_display_start) ? // If start time isn't defined always show until end time has passed
+      const displayStartTime = !timestamp(item.meta_box.event_display_start) ? // If start time isn't defined always show until end time has passed
         0
-        : item.meta_box.event_display_start
-      const displayEndTime = !parseInt(item.meta_box.event_display_end) ?// If end time isn't defined always show when start date has passed
+        : timestamp(item.meta_box.event_display_start)
+      const displayEndTime = !timestamp(item.meta_box.event_display_end) ?// If end time isn't defined always show when start date has passed
         2147483647
-        : item.meta_box.event_display_end
-      const timeNow = Math.round(Date.now() / 1000)
+        : timestamp(item.meta_box.event_display_end)
+      const timeNow = timestamp()
 
       if (timeNow < displayStartTime || timeNow > displayEndTime) {
         return false
