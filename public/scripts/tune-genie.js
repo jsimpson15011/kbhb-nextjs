713aa2a8362bf7509c181bfa7ef59b27ca820348
Joseph Simpson <joseph.simpson@thehomeslicegroup.com>
2020-01-02 10:29:46 -0700

    Add tune genie media player

--- public/scripts/tune-genie.js
+++ public/scripts/tune-genie.js
@@ -1,13 +1,12 @@
-/*
 window.addEventListener('load', function () {
-  jQuery = ''
+  const jQuery = null
   window.tgmplibs = "media2bar"
-  top.tgmp ? top.tgmp.onload() : window.tgmp = new TuneGenieMediaPlayer("playerbar", {
+  window.tgmp = new TuneGenieMediaPlayer("playerbar", {
     brand: "kout",
     theme: ["#000000"],
     position: "bottom",
     autostart: false,
     infoTrayOnLoad: false,
-    frame: true
+    frame: false
   })
-})*/
+})
