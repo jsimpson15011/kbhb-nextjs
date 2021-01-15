d0c40b131ece9afcc66e56c4b07a35501b764999
Joseph Simpson <joseph.simpson@thehomeslicegroup.com>
2021-01-15 15:43:41 -0700

    Add structured data to articles

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -15,9 +15,12 @@ const NewsPage = props => {
       <h2>Loading...</h2>
     )
   }
-  const {articles, isLoading, isError} = useArticles({url: `${baseUrl}/wp-json/wp/v2/posts?slug=${router.query.slug}`,initialData: props.articles})
+  const {articles, isLoading, isError} = useArticles({
+    url: `${baseUrl}/wp-json/wp/v2/posts?slug=${router.query.slug}`,
+    initialData: props.articles
+  })
 
-  if (isLoading){
+  if (isLoading) {
     return (
       <h2>Loading...</h2>
     )
@@ -30,21 +33,43 @@ const NewsPage = props => {
     <MainLayout>
       <Head>
         <title>{siteTitle} - {articles[0].title.rendered}</title>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org/",
+              "@type": "Article",
+              "datePublished": articles[0]["date_gmt"],
+              "headline": articles[0].title.rendered,
+              "image": articles[0].images[0] ? articles[0].images[0].news_photo_full[0] : "/img/logo.png"
+/*              "author": {
+                "@type": "Organization",
+                "name": "unknown"
+              },
+              "publisher": {
+                "@type": "Organization",
+                "name": "KBHB Ranch Radio"
+              }*/
+            })
+          }}
+        />
+
+
         <link rel='icon' href='/favicon.ico'/>
         <meta property="og:url"
-              content={`https://www.kbhbradio.com${router.asPath}`} />
+              content={`https://www.kbhbradio.com${router.asPath}`}/>
         <meta property="og:type" content="article"/>
         <meta property="og:title" content={articles[0].title.rendered}/>
         {
           articles[0].images[0] ?
             <>
-            <meta property="og:image"
-                  content={articles[0].images[0].news_photo_full[0]}/>
-            <meta property="og:width" content={articles[0].images[0].news_photo_full[1]}/>
-          <meta property="og:height" content={articles[0].images[0].news_photo_full[2]}/>
-          </>
-          :
-          ""
+              <meta property="og:image"
+                    content={articles[0].images[0].news_photo_full[0]}/>
+              <meta property="og:width" content={articles[0].images[0].news_photo_full[1]}/>
+              <meta property="og:height" content={articles[0].images[0].news_photo_full[2]}/>
+            </>
+            :
+            ""
         }
 
       </Head>
@@ -64,7 +89,7 @@ export async function getStaticPaths() {
   const posts = await fetcher(`${baseUrl}/wp-json/wp/v2/posts?per_page=100`)
 
   const paths = posts.map(post => ({
-    params: { slug: post.slug }
+    params: {slug: post.slug}
   }))
 
   return {
