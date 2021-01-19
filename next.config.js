669fa7007a1afb156918a789cc80dbe5194d1f21
Joseph Simpson <joseph.simpson@thehomeslicegroup.com>
2021-01-19 10:52:43 -0700

    Add redirects

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   images: {
     domains: ['kbhbcms.homesliceweb.com','blackhillsstore.com','api.weather.gov']
+  },
+  async redirects() {
+    return [
+      {
+        source: '/obituaries',
+        destination: '/category/obituaries',
+        permanent: false
+      },
+      {
+        source: '/news',
+        destination: '/category/news',
+        permanent: false
+      }
+    ]
   }
 }
