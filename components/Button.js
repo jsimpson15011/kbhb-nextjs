6778e9904172b5ba766ba3c1017235deb43fd605
Joseph Simpson <joseph.simpson@thehomeslicegroup.com>
2021-02-03 09:39:33 -0700

    Improve button styling

--- components/Button.js
+++ components/Button.js
@@ -35,44 +35,50 @@ const Button = (props) => {
 
             <style jsx>
               {`
-              a{
-                text-decoration: none;
-                background: ${props.outline ? "none" : mainTheme.accent};
-                color: white;
-                margin-bottom: 14px;
-                padding: 5px 0;
-                width: ${props.width ? props.width : "auto"};
-                max-width: 100%;
-                display: block;
-                text-align: center;
-                font-size: 1.1rem;
-                font-family: 'Montserrat', sans-serif;
-                text-transform: uppercase;
-                letter-spacing: 2px;
-                border-radius: 7px;
-                margin-left: ${props.spacing};
-                margin-right: ${props.spacing};
-                border: ${props.outline ? "solid 2px white" : ""};
-              }
-              a:hover, a:focus{
-                background: white;
-                border: ${props.outline ? "solid 2px" + mainTheme.accent : ""};
-                color: ${mainTheme.accent};
-              }
-              a:hover .button__overline, a:focus .button__overline{
-                color: ${mainTheme.accent};
-              }
-              @media all and (min-width: 520px) {
-                font-size: 1.1rem;
-              }
-              .button__overline{
-                font-weight:bold;
-              }
-              .button-container{
-                display: flex;
-                flex-shrink: 1;
-              }
-`}
+                a {
+                  text-decoration: none;
+                  background: ${props.outline ? "none" : mainTheme.accent};
+                  color: white;
+                  margin-bottom: 14px;
+                  padding: 5px 0;
+                  width: ${props.width ? props.width : "auto"};
+                  max-width: 100%;
+                  display: block;
+                  text-align: center;
+                  font-size: 1.1rem;
+                  font-family: 'Montserrat', sans-serif;
+                  text-transform: uppercase;
+                  letter-spacing: 2px;
+                  border-radius: 7px;
+                  margin-left: ${props.spacing};
+                  margin-right: ${props.spacing};
+                  border: ${props.outline ? "solid 2px white" : ""};
+                  transition: color .2s, background-color .2s;
+                }
+
+                a:hover, a:focus {
+                  background: #f7f9fc;
+                  border: ${props.outline ? "solid 2px" + mainTheme.accent : ""};
+                  color: ${mainTheme.accent};
+                }
+
+                a:hover .button__overline, a:focus .button__overline {
+                  color: ${mainTheme.accent};
+                }
+
+                @media all and (min-width: 520px) {
+                  font-size: 1.1rem;
+                }
+
+                .button__overline {
+                  font-weight: bold;
+                }
+
+                .button-container {
+                  display: flex;
+                  flex-shrink: 1;
+                }
+              `}
             </style>
           </div>
 
